ab56ed195c6055144f7e5a4c84ab3b8b1c94c731
Anatoliy Ignatev <anatoliyignatev@gmail.com>
2019-11-27 21:28:02 +0300

    Update ModelViewBase

--- TC-generator/TC-generator/ViewModel/ViewModelBase.cs
+++ TC-generator/TC-generator/ViewModel/ViewModelBase.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TC_generator.ViewModel
 {
-    public class ViewModelBase
+    public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
     {
+        protected ViewModelBase()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChangedEventHandler = this.PropertyChanged;
+
+            propertyChangedEventHandler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        protected virtual void OnDispose()
+        {
+
+        }
+
     }
 }
