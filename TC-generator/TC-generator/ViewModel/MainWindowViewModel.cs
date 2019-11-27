704d6dabbbadf3a65214f1776ff7619db214df9c
Anatoliy Ignatev <anatoliyignatev@gmail.com>
2019-11-27 22:24:16 +0300

    Add Window.DataContext

--- TC-generator/TC-generator/ViewModel/MainWindowViewModel.cs
+++ TC-generator/TC-generator/ViewModel/MainWindowViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TC_generator.ViewModel
 {
-    class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase
     {
     }
 }
