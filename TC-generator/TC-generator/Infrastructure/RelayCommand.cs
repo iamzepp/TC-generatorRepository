aa23a3c2e687275c4e17d2687b2c0695208ee456
Anatoliy Ignatev <anatoliyignatev@gmail.com>
2019-11-27 21:51:47 +0300

    Add RelayCommand class

---
+++ TC-generator/TC-generator/Infrastructure/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TC_generator.Infrastructure
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> execute;
+        readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute.Invoke(parameter);
+        }
+    }
+}
