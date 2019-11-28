e322a42fdc2110c7231da3b3dc63f597a2678236
Anatoliy Ignatev <anatoliyignatev@gmail.com>
2019-11-28 22:14:05 +0300

    Add XML input type

---
+++ TC-generator/TC-generator/Model/InputObjects/XmlInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TC_generator.Model.InputObjects
+{
+    public class XmlInput : IInput
+    {
+        public InputInfo GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
